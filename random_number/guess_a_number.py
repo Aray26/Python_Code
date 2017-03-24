4e3c8fb478979536f000b47ffdadd9a4d264514b
Aray26 <adi.ray@gmail.com>
2017-03-24 09:51:38 -0400

    Add files via upload

--- random_number/guess_a_number.py
+++ random_number/guess_a_number.py
@@ -1,1 +1,17 @@
-xs
+import random
+n = 20
+to_be_guessed = int(n * random.random()) + 1
+guess = 0
+print "enter in 0 to give up"
+while guess != to_be_guessed:
+    guess = input("New number: ")
+    if guess > 0:
+        if guess > to_be_guessed:
+            print "Number too large"
+        elif guess < to_be_guessed:
+            print "Number too small"
+    else:
+        print "Sorry that you're giving up!"
+        break
+else:
+    print "Congratulation. You made it!"
