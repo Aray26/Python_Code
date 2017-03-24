b90eae2d3eca136ee63311fcf1946ad5cb07940a
Aray26 <adi.ray@gmail.com>
2017-03-24 17:02:12 -0400

    Update string_formating.py

--- string_formating/string_formating.py
+++ string_formating/string_formating.py
@@ -72,4 +72,72 @@ print("Countries and their capitals:")
 for c in capital_country:
     format_string = c + ": {" + c + "}"
     print(format_string.format(**capital_country))
+    
+#Other string methods for Formatting
+#The string class contains further methods, which can be used for formatting purposes as well: ljust, rjust, center and zfill
+
+#Let S be a string, the 4 methods are defined like this:
+'''
+S.center(width[, fillchar]) -> str
+
+    Return S centred in a string of length width. Padding is done using the specified fill character. The default value is a space.
+
+    Examples:
+
+    >>> s = "Python"
+    >>> s.center(10)
+    '  Python  '
+    >>> s.center(10,"*")
+    '**Python**'
+
+    ljust(...):
+
+     S.ljust(width[, fillchar]) -> str 
+
+    Return S left-justified in a string of length "width". Padding is done using the specified fill character. If none is given, a space will be used as default.
+
+    Examples:
+
+    >>> s = "Training"
+    >>> s.ljust(12)
+    'Training    '
+    >>> s.ljust(12,":")
+    'Training::::'
+    >>> 
+
+    rjust(...):
+
+    S.rjust(width[, fillchar]) -> str
+
+    Return S right-justified in a string of length width. Padding is done using the specified fill character. The default value is again a space.
+
+    Examples:
+
+    >>> s = "Programming"
+    >>> s.rjust(15)
+    '    Programming'
+    >>> s.rjust(15, "~")
+    '~~~~Programming'
+    >>> 
+
+    zfill(...):
+
+     
+    S.zfill(width) -> str
+
+    Pad a string S with zeros on the left, to fill a field of the specified width. The string S is never truncated. This method can be easily emulated with rjust.
+
+    Examples:
+
+    >>> account_number = "43447879"
+    >>> account_number.zfill(12)
+    '000043447879'
+    >>> # can be emulated with rjust:
+    ... 
+    >>> account_number.rjust(12,"0")
+    '000043447879'
+    >>> 
+'''
+print "close this file"
+
 
