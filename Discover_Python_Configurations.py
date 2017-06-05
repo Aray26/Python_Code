aec7909fb653691271ef6617ee99be9f5d63bf89
Aray26 <adi.ray@gmail.com>
2017-06-05 17:11:02 -0400

    Update Discover_Python_Configurations.py

--- Discover_Python_Configurations.py
+++ Discover_Python_Configurations.py
@@ -1,7 +1,11 @@
 import sys
+import nltk
+import sklearn
 
 print ("Configurations")
 print (sys.version) + str("\n")
 print str(sys.version_info) + str("\n")
 print (sys.path)
 
+print('The nltk version is {}.'.format(nltk.__version__))
+print('The scikit-learn version is {}.'.format(sklearn.__version__))
